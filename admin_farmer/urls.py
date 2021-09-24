2b8aa72a0bfb6ac47cfcf0c726c6e92fc4a7050e
Tchamgoue <adrienne.tchamgoue@gmail.com>
2021-09-24 12:09:10 +0100

    list of crop requests (views)

--- admin_farmer/urls.py
+++ admin_farmer/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     url('list_farmers', views.list_farmers_view, name='list_farmers_view'),
     url('farmer_location', views.farmer_location_view, name='farmer_location_view'),
     url('add_crop_request', views.add_crop_request_view, name='add_crop_request_view'),
-    url('list_crop_request', views.list_crop_request_view, name='list_crop_request_view'),
+    url('list_crop_requests', views.list_crop_requests_view, name='list_crop_requests_view'),
 
 ]
